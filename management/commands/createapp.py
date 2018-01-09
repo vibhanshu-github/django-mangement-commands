4f07fd70003117bda6d8947111e64b4491da8ec5
vibhanshu-github <vibhanshu909@gmail.com>
2018-01-09 13:24:15 +0530

    Added support for windows

--- management/commands/createapp.py
+++ management/commands/createapp.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
 
         import os
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-        FILE_PATH = os.path.join(BASE_DIR,BASE_DIR.split('/')[-1]+'/settings.py')
+        FILE_PATH = os.path.join(BASE_DIR,os.path.split(BASE_DIR)[-1],'settings.py')
         with open(FILE_PATH, "r") as f:
             contents = f.readlines()
 
@@ -29,13 +29,13 @@ class Command(BaseCommand):
                         if os.path.isdir(app_path) is True:
                             self.stdout.write(app_path)
                             """ Code to create static folder hierarichy """
-                            os.makedirs(os.path.join(app_path,"static/"+app),exist_ok=True)
-                            os.makedirs(os.path.join(app_path,"static/"+app+"/css"),exist_ok=True)
-                            os.makedirs(os.path.join(app_path,"static/"+app+"/css/fonts"),exist_ok=True)
-                            os.makedirs(os.path.join(app_path,"static/"+app+"/js"),exist_ok=True)
-                            os.makedirs(os.path.join(app_path,"static/"+app+"/img"),exist_ok=True)
+                            os.makedirs(os.path.join(app_path,"static",app),exist_ok=True)
+                            os.makedirs(os.path.join(app_path,"static",app,"css"),exist_ok=True)
+                            os.makedirs(os.path.join(app_path,"static",app,"css","fonts"),exist_ok=True)
+                            os.makedirs(os.path.join(app_path,"static",app,"js"),exist_ok=True)
+                            os.makedirs(os.path.join(app_path,"static",app,"img"),exist_ok=True)
                             """ Code to create templates folder """
-                            os.makedirs(os.path.join(app_path,"templates/"+app),exist_ok=True)
+                            os.makedirs(os.path.join(app_path,"templates",app),exist_ok=True)
                         else:
                             raise CommandError("'%s' app does not exist" % app)
                         app = "    '"+app+"',\n"
